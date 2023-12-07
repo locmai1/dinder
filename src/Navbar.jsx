6dd6ddf6071df4457928420401fc0bc13227ec0a
wkim10 <wkim10@tufts.edu>
2023-12-06 20:30:13 -0500

    set up routes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,25 +1,25 @@
 import "./styles/Navbar.css";
 
 export default function Navbar() {
-	return (
-		<nav class="navbar">
-			<div class="logo">
-				din<span style={{ color: "#FFD233" }}>der</span>
-			</div>
-			<ul class="nav-links">
-				<li>
-					<a href="/Explore">Explore</a>
-				</li>
-				<li>
-					<a href="/Hosting">Hosting</a>
-				</li>
-				<li>
-					<a href="/Pending">Pending</a>
-				</li>
-				<li>
-					<a href="/Profile">Profile</a>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <a className="logo" href="/">
+        din<span style={{ color: "#FFD233" }}>der</span>
+      </a>
+      <ul className="nav-links">
+        <li>
+          <a href="/">Explore</a>
+        </li>
+        <li>
+          <a href="/hosting">Hosting</a>
+        </li>
+        <li>
+          <a href="/joining">Joining</a>
+        </li>
+        <li>
+          <a href="/profile">Profile</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
