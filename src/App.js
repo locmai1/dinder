a193e8f27a316ff9fc3e078e6a6e369fef4f6552
wkim10 <wkim10@tufts.edu>
2023-12-09 15:19:36 -0500

    small style fixes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Explore />} />
-				<Route path="/hosting" element={<Hosting />} />
-				<Route path="/joining" element={<Joining />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/signup" element={<SignUp />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Explore />} />
+        <Route path="/hosting" element={<Hosting />} />
+        <Route path="/joining" element={<Joining />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
