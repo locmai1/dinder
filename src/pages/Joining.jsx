b9aa628d4324a01a13a4527187898a23f72f5908
ffft1215 <fa.t4510@gmail.com>
2023-12-10 16:44:45 -0500

    changed formatting on cards for explore, hosting, joining pages

--- src/pages/Joining.jsx
+++ src/pages/Joining.jsx
@@ -2,107 +2,107 @@ import "../styles/Joining.css";
 import Navbar from "../components/Navbar";
 
 export default function Joining() {
-  return (
-    <div>
-      <Navbar />
-      <div className="Joining">
-        <div className="heading">Joining</div>
-        <div className="sub-heading">Confirmed dinders</div>
+	return (
+		<div>
+			<Navbar />
+			<div className="Joining">
+				<div className="heading">Joining</div>
+				<div className="sub-heading">Confirmed dinders</div>
 
-        <div style={{ display: "flex", flexDirection: "row", gap: "25px" }}>
-          <div className="event">
-            <div className="event-head-joining">
-              <div className="event-title-hours">
-                <div className="event-title">Dinner @ 6</div>
-                <div className="event-hours-ago">2 hrs ago</div>
-              </div>
-              <div>Anna L.</div>
-            </div>
+				<div className="format-joining">
+					<div className="event">
+						<div className="event-head-joining">
+							<div className="event-title-hours">
+								<div className="event-title">Dinner @ 6</div>
+								<div className="event-hours-ago">2 hrs ago</div>
+							</div>
+							<div>Anna L.</div>
+						</div>
 
-            <div className="event-grid">
-              <div>Date </div>
-              <div className="event-content">15 March</div>
-              <div>Type</div>
-              <div className="event-content">1 on 1</div>
-              <div>Location </div>
-              <div className="event-content">Carm</div>
-              <div>Purpose</div>
-              <div className="event-content">Advice</div>
-            </div>
+						<div className="event-grid">
+							<div>Date </div>
+							<div className="event-content">15 March</div>
+							<div>Type</div>
+							<div className="event-content">1 on 1</div>
+							<div>Location </div>
+							<div className="event-content">Carm</div>
+							<div>Purpose</div>
+							<div className="event-content">Advice</div>
+						</div>
 
-            <div className="meeting-location">
-              <div>Meeting Location</div>
-              <div style={{ fontWeight: "700" }}>574 Boston Ave.</div>
-            </div>
+						<div className="meeting-location">
+							<div>Meeting Location</div>
+							<div style={{ fontWeight: "700" }}>574 Boston Ave.</div>
+						</div>
 
-            <div className="chat-button">
-              <img src="img/chat.svg" alt="Chat" />
-              <div>Chat</div>
-            </div>
-          </div>
+						<div className="chat-button">
+							<img src="img/chat.svg" alt="Chat" />
+							<div>Chat</div>
+						</div>
+					</div>
 
-          <div className="event">
-            <div className="event-head-joining">
-              <div className="event-title-hours">
-                <div className="event-title">Dinner @ 6</div>
-                <div className="event-hours-ago">2 hrs ago</div>
-              </div>
-              <div>Anna L.</div>
-            </div>
+					<div className="event">
+						<div className="event-head-joining">
+							<div className="event-title-hours">
+								<div className="event-title">Dinner @ 6</div>
+								<div className="event-hours-ago">2 hrs ago</div>
+							</div>
+							<div>Anna L.</div>
+						</div>
 
-            <div className="event-grid">
-              <div>Date </div>
-              <div className="event-content">15 March</div>
-              <div>Type</div>
-              <div className="event-content">1 on 1</div>
-              <div>Location </div>
-              <div className="event-content">Carm</div>
-              <div>Purpose</div>
-              <div className="event-content">Advice</div>
-            </div>
+						<div className="event-grid">
+							<div>Date </div>
+							<div className="event-content">15 March</div>
+							<div>Type</div>
+							<div className="event-content">1 on 1</div>
+							<div>Location </div>
+							<div className="event-content">Carm</div>
+							<div>Purpose</div>
+							<div className="event-content">Advice</div>
+						</div>
 
-            <div className="meeting-location">
-              <div>Meeting Location</div>
-              <div style={{ fontWeight: "700" }}>574 Boston Ave.</div>
-            </div>
+						<div className="meeting-location">
+							<div>Meeting Location</div>
+							<div style={{ fontWeight: "700" }}>574 Boston Ave.</div>
+						</div>
 
-            <div className="chat-button">
-              <img src="img/chat.svg" alt="Chat" />
-              <div>Chat</div>
-            </div>
-          </div>
-        </div>
+						<div className="chat-button">
+							<img src="img/chat.svg" alt="Chat" />
+							<div>Chat</div>
+						</div>
+					</div>
+				</div>
 
-        <div className="sub-heading">Pending Requests</div>
+				<div className="sub-heading">Pending Requests</div>
 
-        <div className="event">
-          <div className="event-head-joining">
-            <div className="event-title-hours">
-              <div className="event-title">Dinner @ 6</div>
-              <div className="event-hours-ago">2 hrs ago</div>
-            </div>
-            <div>Anna L.</div>
-          </div>
+				<div className="event">
+					<div className="event-head-joining">
+						<div className="event-title-hours">
+							<div className="event-title">Dinner @ 6</div>
+							<div className="event-hours-ago">2 hrs ago</div>
+						</div>
+						<div>Anna L.</div>
+					</div>
 
-          <div className="event-grid">
-            <div>Date </div>
-            <div className="event-content">15 March</div>
-            <div>Type</div>
-            <div className="event-content">1 on 1</div>
-            <div>Location </div>
-            <div className="event-content">Carm</div>
-            <div>Purpose</div>
-            <div className="event-content">Advice</div>
-          </div>
+					<div className="event-grid">
+						<div>Date </div>
+						<div className="event-content">15 March</div>
+						<div>Type</div>
+						<div className="event-content">1 on 1</div>
+						<div>Location </div>
+						<div className="event-content">Carm</div>
+						<div>Purpose</div>
+						<div className="event-content">Advice</div>
+					</div>
 
-          <div className="meeting-location">
-            <div>Meeting Location</div>
-            <div style={{ fontWeight: "700" }}>574 Boston Ave.</div>
-          </div>
+					<div className="meeting-location">
+						<div>Meeting Location</div>
+						<div style={{ fontWeight: "700" }}>574 Boston Ave.</div>
+					</div>
 
-          <div className="cancel-button">Cancel Request</div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className="cancel-button">Cancel Request</div>
+				</div>
+			</div>
+		</div>
+	);
 }
