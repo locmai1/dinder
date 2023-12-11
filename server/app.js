f206bdbb4d343fdb7d6e8a8ca1df7bfe5e82816c
Loc Mai <locmai0315@gmail.com>
2023-12-10 22:51:57 -0500

    dada

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
